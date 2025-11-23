15061e9bf1e175de9704f996c283c7137c45696a
Mickael da Costa <dribeirodacosta@pagerduty.com>
2025-11-23 21:35:30 +0000

    Add the backend

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ const customFrontendUrl = process.env.FRONTEND_URL || '';
 
 const allowedOrigins = [
   customFrontendUrl,
+  'https://pwa-all-app-backend.onrender.com',
   "http://*:*"
 ].filter(Boolean);
 
